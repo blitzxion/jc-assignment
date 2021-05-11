39c8b1f242153ac11936c60195471ce495d55382
Richard T. Shaw <richard.shaw@theactigraph.com>
2021-05-11 15:02:56 -0500

    Updated console application to execute library as intended.

--- JumpCloudAssignment/Program.cs
+++ JumpCloudAssignment/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using JumpCloudAssignment.Services;
+
 namespace JumpCloudAssignment
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var service = new ActionService();
+
+            var errorStr = "";
+            foreach (var arg in args)
+            {
+                errorStr = service.AddAction(arg);
+                if (false == string.IsNullOrEmpty(errorStr))
+                    break;
+            }
+
+            if (false == string.IsNullOrEmpty(errorStr))
+            {
+                Console.WriteLine($"ERROR: Unable to add one or more actions -> {errorStr}");
+                return;
+            }
+
+            var stats = service.GetStats();
+
+            Console.WriteLine(stats);
+
+            return;
         }
     }
 }
