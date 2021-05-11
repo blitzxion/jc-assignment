03bb5e0b49e128926dace57f0e6d61d1ec184d1a
Richard T. Shaw <richard.shaw@theactigraph.com>
2021-05-11 18:12:10 -0500

    Replaced batch files to perform same logic as powershell.

--- build.bat
+++ build.bat
@@ -1,1 +1,12 @@
-powershell .\build.ps1
+@echo off
+cls
+SET ngt=".\tools\nuget.exe"
+SET msb="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
+
+echo Restoring packages...
+%ngt% restore 
+echo All packages restored.
+
+echo Compiling solution...
+%msb% JumpCloudAssignment.sln /m /t:Rebuild /p:Configuration=Release 
+echo Compile complete.
--- runtests.bat
+++ runtests.bat
@@ -1,1 +1,19 @@
-powershell .\runtests.ps1
+@echo off
+cls
+
+SET ngt=".\tools\nuget.exe"
+SET msb="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\msbuild.exe"
+SET test=".\packages\xunit.runner.console.2.4.1\tools\net472\xunit.console.exe"
+SET testPath=".\JumpCloudAssignment.Tests\bin\Release\JumpCloudAssignment.Tests.dll"
+
+echo Restoring packages...
+%ngt% restore -noninteractive -verbosity quiet
+echo All packages restored.
+
+echo Compiling solution...
+%msb% JumpCloudAssignment.sln /m /nologo /verbosity:quiet /t:Rebuild /p:Configuration=Release
+echo Compile complete.
+
+echo Starting tests...
+%test% %testPath% -nologo -verbose
+echo Tests complete.
